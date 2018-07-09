720db33b8f4ebb976ce9e53f96498b425950a8d1
myfreeer <myfreeer@users.noreply.github.com>
2018-07-09 16:50:47 +0800

    make_chm: add gbk making and packing script
    remove the standalone make_gbk.sh

--- make_chm.sh
+++ make_chm.sh
@@ -14,6 +14,14 @@ do
       HHC="${i#*=}"
       shift # past argument=value
     ;;
+    _7Z=*)
+      _7Z="${i#*=}"
+      shift # past argument=value
+    ;;
+    VERSION=*)
+      VERSION="${i#*=}"
+      shift # past argument=value
+    ;;
     *)
       # unknown option
     ;;
@@ -23,6 +31,8 @@ done
 PHP="${PHP:-$(which php)}"
 ICONV="${ICONV:-$(which iconv)}"
 HHC="${HHC:-$(which hhc)}"
+_7Z="${_7Z:-$(which 7z)}"
+VERSION="${VERSION:-$(date +%Y%m)}"
 CPUS="$(cat /proc/cpuinfo | grep -c '^processor')"
 
 # 在执行下一步之前，移动reference或output目录下文件到当前目录
@@ -47,4 +57,25 @@ mv "${i}" "chm_temp/${i}"
 done
 rm -rf chm_temp
 
-"${HHC}" cppreference.hhp
+sed "s/cppreference\.chm/cppreference-zh-${VERSION}\.chm/" cppreference.hhp > "cppreference-zh-${VERSION}.hhp"
+
+# Compile and package UTF-8 version
+"${HHC}" "cppreference-zh-${VERSION}.hhp"
+"${_7Z}" a -mx9 "cppreference-zh-${VERSION}-chm-project.7z" "cppreference-zh-${VERSION}.hhp" cppreference.{hhc,hhk} hh{a.dll,c.exe} chmhelp/*
+
+# Convert HTML to GBK
+mv chmhelp chmhelp1
+mkdir -p chmhelp
+cd chmhelp1
+
+find -iname '*.html' | xargs -P "${CPUS}" sed -i 's/<meta http-equiv="Content-Type" content="text\/html; charset=utf-8" \/>/<meta http-equiv="Content-Type" content="text\/html; charset=gbk" \/>/'
+ls | grep .html | xargs -P "${CPUS}" -I {} sh -c "${ICONV} -c -f UTF-8 -t GBK '{}' > '../chmhelp/{}'"
+cp -n ./* ../chmhelp/
+cd ..
+rm -rf chmhelp1
+
+sed "s/cppreference\.chm/cppreference-zh-${VERSION}-gbk\.chm/" cppreference.hhp > "cppreference-zh-${VERSION}-gbk.hhp"
+
+# Compile and package GBK version
+"${HHC}" "cppreference-zh-${VERSION}-gbk.hhp"
+"${_7Z}" a -mx9 "cppreference-zh-${VERSION}-chm-project-gbk.7z" "cppreference-zh-${VERSION}-gbk.hhp" cppreference.{hhc,hhk} hh{a.dll,c.exe} chmhelp/*
